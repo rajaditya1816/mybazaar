007414d49bab258e6fd802152a1a3a6262f881e3
raj aditya <rajaditya1816@gmail.com>
2022-05-09 16:32:25 +0530

    gaurav

--- src/Aboutus.js
+++ src/Aboutus.js
@@ -2,13 +2,107 @@ import React from "react";
 import "./Aboutus.css";
 
 function Aboutus() {
-    return(
-        <div className="about">
-            <h1>hey...... user!</h1>
+  return (
+    <div className="about">
+      <div className="image">
+        <img src="https://cdn.pixabay.com/photo/2017/03/13/17/26/ecommerce-2140603_960_720.jpg" />
+      </div>
+      <div className="head">
+        <h1>
+          *****HELLO GUEST, Welcome to a new era of shopping experiance.*****
+        </h1>
+        <h2>This WEB APPLICATION is created and developed by 'RAJ ADITYA'</h2>
+      </div>
+      <div className="section">
+        <div className="first">
+          <h2>some inportent websites used in this application</h2>
+          <h3>There are many useful sites used in this web application.</h3>
         </div>
-
-
-
-    );
+        <div className="sections">
+          <div className="ek">
+            <h1>1. Bootstrap</h1>
+            <p>
+              Bootstrap is a free and open-source CSS framework directed at
+              responsive, mobile-first front-end web development. It contains
+              HTML, CSS and JavaScript-based design templates for typography,
+              forms, buttons, navigation, and other interface components.
+            </p>
+            <a href="https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)">
+              Link for description
+            </a>
+          </div>
+          <div className="do">
+            <h1>2. W3school</h1>
+            <p>
+              W3Schools is a freemium educational website for learning coding
+              online. Developed in 1998, it derives its name from the World Wide
+              Web but is not affiliated with the W3C. W3Schools offers courses
+              covering all aspects of web development. It is run by Refsnes Data
+              in Norway
+            </p>
+            <a href="https://en.wikipedia.org/wiki/W3Schools">
+              Link for description
+            </a>
+          </div>
+        </div>
+        <div className="sections">
+          <div className="teen">
+            <h1>3. React JS</h1>
+            <p>
+              React is a free and open-source front-end JavaScript library for
+              building user interfaces based on UI components. It is maintained
+              by Meta and a community of individual developers and companies.
+            </p>
+            <a href="https://reactjs.org/">Link for description</a>
+          </div>
+          <div className="char">
+            <h1>4. Node JS</h1>
+            <p>
+              Node.js is an open-source, cross-platform, back-end JavaScript
+              runtime environment that runs on the V8 engine and executes
+              JavaScript code outside a web browser.{" "}
+            </p>
+            <a href="https://nodejs.org/en/">Link for description</a>
+          </div>
+        </div>
+        <div className="sections">
+          <div className="teen">
+            <h1>5. Firebase</h1>
+            <p>
+              Firebase is a platform developed by Google for creating mobile and
+              web applications. It was originally an independent company founded
+              in 2011. In 2014, Google acquired the platform and it is now their
+              flagship offering for app development
+            </p>
+            <a href="https://firebase.google.com/?gclid=CjwKCAjw9-KTBhBcEiwAr19ig8p9KWZbqxOYoZghWk_UOdFsE7mq0jhEtqOWkmUtqIB5aTDEKIWfXxoCNVoQAvD_BwE&gclsrc=aw.ds">
+              Link for description
+            </a>
+          </div>
+          <div className="char">
+            <h1>6. vercel</h1>
+            <p>
+              Vercel combines the best developer experience with an obsessive
+              focus on end-user performance. Our platform enables frontend teams
+              to do their best work.
+            </p>
+            <a href="https://vercel.com/dashboard">Link for description</a>
+          </div>
+        </div>
+      </div>
+      <div className="develop">
+        <h1>About Developer.</h1>
+        <div className="links">
+          <a href="https://www.facebook.com/profile.php?id=100067139606076">
+            *FACEBOOK*
+          </a>
+          <a href="https://www.instagram.com/rajaditya_2002/?hl=en">
+            *INSTAGRAM*
+          </a>
+          <a href="https://twitter.com/i/flow/login">*TWITTER*</a>
+          <a href="https://github.com/rajaditya1816/mybazaar">*GITHUB*</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Aboutus;
--- src/Home.js
+++ src/Home.js
@@ -70,14 +70,14 @@ function Home() {
           <Product
             id="49038599"
             title="boAt Airdopes 121v2 True Wireless Earbuds (Active Black)"
-            price={1199.00}
+            price={1199.0}
             rating={4}
             image="https://m.media-amazon.com/images/I/510+wjzq-vL._SY450_.jpg"
           />
           <Product
             id="23445967"
             title="V-Guard Zenora RO+UF+MB 7 Litre Water Purifier With 7 Stage Purification and pH Balancer"
-            price={8490.00}
+            price={8490.0}
             rating={4}
             image="https://m.media-amazon.com/images/I/61JIQpSQDtL._SX466_.jpg"
           />
@@ -91,7 +91,7 @@ function Home() {
           <Product
             id="152648530"
             title="GNC Pro Performance 100% Whey Protein - 4.4 lbs, 2 kg (Chocolate Fudge)"
-            price={4724.00}
+            price={4724.0}
             rating={5}
             image="https://m.media-amazon.com/images/I/817CqHqLIQL._SX679_.jpg"
           />
@@ -100,7 +100,7 @@ function Home() {
           <Product
             id="90829999"
             title="HP Chromebook x360 Intel Celeron N4020 Processor 12 inches Touchscreen Laptop, 4GB/64GB eMMC Storage/Chrome OS/1.35kg (Natural Silver), 12b-ca0010TU"
-            price={36499.00}
+            price={36499.0}
             rating={4}
             image="https://m.media-amazon.com/images/I/71WDdKKrkEL._SX679_.jpg"
           />
@@ -109,7 +109,7 @@ function Home() {
           <Product
             id="1232134192"
             title="HP Chromebook 14 Intel Celeron N4020-4GB SDRAM/64GB eMMC + 256GB Expandable Storage 14inch(35.6 cm) Thin & Light Touchscreen Laptop "
-            price={25599.00}
+            price={25599.0}
             rating={5}
             image="https://m.media-amazon.com/images/I/71N0+IGOsLL._SX679_.jpg"
           />
@@ -119,6 +119,121 @@ function Home() {
             price={699.0}
             rating={4}
             image="https://m.media-amazon.com/images/I/61yfVFby5dL._UY741_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="123213419"
+            title="Book--The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094213"
+            title="GoSriKi Women's Cotton Straight Kurta with Palazzo & Dupatta"
+            price={699.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61yfVFby5dL._UY741_.jpg"
+          />
+          <Product
+            id="4953809421"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="123213419"
+            title="Book--The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094213"
+            title="GoSriKi Women's Cotton Straight Kurta with Palazzo & Dupatta"
+            price={699.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61yfVFby5dL._UY741_.jpg"
+          />
+          <Product
+            id="4953809421"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="123213419"
+            title="Book--The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094213"
+            title="GoSriKi Women's Cotton Straight Kurta with Palazzo & Dupatta"
+            price={699.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61yfVFby5dL._UY741_.jpg"
+          />
+          <Product
+            id="4953809421"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="123213419"
+            title="Book--The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094213"
+            title="GoSriKi Women's Cotton Straight Kurta with Palazzo & Dupatta"
+            price={699.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61yfVFby5dL._UY741_.jpg"
+          />
+          <Product
+            id="4953809421"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="123213419"
+            title="Book--The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094213"
+            title="GoSriKi Women's Cotton Straight Kurta with Palazzo & Dupatta"
+            price={699.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61yfVFby5dL._UY741_.jpg"
+          />
+          <Product
+            id="4953809421"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
           />
         </div>
       </div>
